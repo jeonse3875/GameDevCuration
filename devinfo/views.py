7a21bb0444871177c283334f4bae420a5c7071a0
jeonse3875 <devbyseke@gmail.com>
2022-06-14 07:22:46 +0900

    기능: tree 전체 보기 추가

--- devinfo/views.py
+++ devinfo/views.py
@@ -9,6 +9,7 @@ class DevInformationView(View):
         game_id = int(request.GET.get('game_id',0))
         cur_tag = int(request.GET.get('cur_tag',0))
         sel_tag = list(map(int, request.GET.getlist('sel_tag',[])))
+        is_show_all = bool(request.GET.get('is_show_all',False))
         templateData = {}
         templateData["games"] = Game.objects.all()
 
@@ -32,6 +33,7 @@ class DevInformationView(View):
         templateData["tags"] = targetGame.tags.all()
         templateData["sel_tag"] = sel_tag
         templateData["sel_tag_url"] = sel_tag_url
+        templateData["is_show_all"] = is_show_all
 
         try:
             templateData["cur_tag"] = Tag.objects.get(id=cur_tag)
