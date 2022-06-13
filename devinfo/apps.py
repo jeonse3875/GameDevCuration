8991f24f36413bd8c46cecae54c69e3377cbb693
jeonse3875 <devbyseke@gmail.com>
2022-06-13 21:41:45 +0900

    기능: devinfo 앱 추가
    개발 정보를 모아 보여줄 페이지를 위한 devinfo 앱 추가

---
+++ devinfo/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DevinfoConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'devinfo'
---
+++ devinfo/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse  
+from django.shortcuts import render
+from django.views.generic import View
+
+# Create your views here.
+class DevInformationView(View):
+    def get(self, request):
+        # 뷰 로직 작성
+        return render(request,'home.html')
