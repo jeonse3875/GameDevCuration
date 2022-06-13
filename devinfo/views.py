e443693227c04f219ade592b6cd71864afa3bb48
jeonse3875 <devbyseke@gmail.com>
2022-06-14 03:49:19 +0900

    기능: devinfo tree 단계별 조회 및 접기/펴기 추가

--- devinfo/views.py
+++ devinfo/views.py
@@ -6,17 +6,32 @@ from .models import *
 # Create your views here.
 class DevInformationView(View):
     def get(self, request):
-        game_id = request.GET.get('game_id',0)
-        tag_id = request.GET.get('tag_id',0)
+        game_id = int(request.GET.get('game_id',0))
+        cur_tag = int(request.GET.get('cur_tag',0))
+        sel_tag = list(map(int, request.GET.getlist('sel_tag',[])))
+        
 
         try:
             targetGame = Game.objects.get(id = game_id)
         except Game.DoesNotExist:
+            # 게임 선택 페이지 띄우기
             return render(request,'devinfo/home.html')
+
+        if cur_tag in sel_tag:
+            sel_tag.remove(cur_tag)
+        else:
+            sel_tag.append(cur_tag)
         
+        sel_tag_url = ""
+
+        for tag_id in sel_tag:
+            sel_tag_url += f"&sel_tag={tag_id}"
+
         templateData = {}
         templateData["game"] = targetGame
         templateData["tags"] = targetGame.tags.all()
+        templateData["sel_tag"] = sel_tag
+        templateData["sel_tag_url"] = sel_tag_url
 
-        print(templateData)
+        #print(templateData)
         return render(request,'devinfo/home.html', templateData)
