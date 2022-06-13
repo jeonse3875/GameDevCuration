20112d27b5c92f11888ea03314c08089ff21e34f
jeonse3875 <devbyseke@gmail.com>
2022-06-13 23:02:52 +0900

    기능: Tag, TagRelation 모델 추가
    Tag 모델과 Tag 사이의 계층관계 표현을 위한 TagRelation 모델 추가. admin 페이지를 통해 모델 관리 가능.

--- devinfo/models.py
+++ devinfo/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+    thumbnail = models.ImageField(upload_to="thumbnail",null=True,blank=True)
+    
+    def __str__(self):
+        return self.name
+
+class TagRelation(models.Model):
+    parent = models.ForeignKey(Tag,on_delete=models.CASCADE,null=False,related_name='parent_tag')
+    child = models.ForeignKey(Tag,on_delete=models.CASCADE,null=False,related_name='child_tag')
+
+    def __str__(self):
+        return f'{self.parent} : {self.child}'
