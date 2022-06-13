2967f79c9a811411a5d20ac69cd2f0638ee1ec8b
jeonse3875 <devbyseke@gmail.com>
2022-06-14 05:22:14 +0900

    디자인: 노드 배경 이미지 적용 및 css 수정

--- devinfo/urls.py
+++ devinfo/urls.py
@@ -1,6 +1,8 @@
 from . import views
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static  
 
 urlpatterns = [
     path('', views.DevInformationView.as_view(),name='devinfo'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
